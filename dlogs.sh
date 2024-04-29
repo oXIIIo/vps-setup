09e70d6b962f4ac0f4b726ea7e501596791f306b
oXIIIo <139705681+oXIIIo@users.noreply.github.com>
2024-04-29 16:34:05 +0330

    style: Consistent 2-space indentation and LF endings

--- dlogs.sh
+++ dlogs.sh
@@ -13,18 +13,18 @@ fi
 PS3="Select a container to view its logs or 'q' to quit: "
 select container in $containers q; do
   case $container in
-    q)
-      echo "Exiting..."
-      break
-      ;;
-    *)
-      if [ -n "$container" ]; then
-        # User selected a container
-        echo "Following the last 10 lines of logs for $container..."
-        docker logs -f -n 10 "$container"
-      else
-        echo "Invalid selection. Please choose a valid option."
-      fi
-      ;;
+  q)
+    echo "Exiting..."
+    break
+    ;;
+  *)
+    if [ -n "$container" ]; then
+      # User selected a container
+      echo "Following the last 10 lines of logs for $container..."
+      docker logs -f -n 10 "$container"
+    else
+      echo "Invalid selection. Please choose a valid option."
+    fi
+    ;;
   esac
 done
